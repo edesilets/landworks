34ac8a3cbc01bb9f36c98960ed72128c5bc13203
Ethan Desilets <ethandesilets@gmail.com>
2026-02-07 13:27:51 -0500

    Render Footer and Header

--- app/root.tsx
+++ app/root.tsx
@@ -9,6 +9,8 @@ import {
 
 import type { Route } from "./+types/root";
 import "./app.css";
+import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
 
 export const links: Route.LinksFunction = () => [
 	{ rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -29,11 +31,18 @@ export function Layout({ children }: { children: React.ReactNode }) {
 			<head>
 				<meta charSet="utf-8" />
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<meta name="description" content="Desilets Landworks - Professional Landscaping Services" />
+				<meta name="keywords" content="landscaping,landworks,outdoor" />
 				<Meta />
 				<Links />
+				<title>Desilets Landworks</title>
 			</head>
-			<body>
+			<body className="flex flex-col min-h-screen">
+				<Header />
+				<main className="flex-grow">
 				{children}
+				</main>
+				<Footer />
 				<ScrollRestoration />
 				<Scripts />
 			</body>
