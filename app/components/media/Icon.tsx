37e34ae2582257b77ebf7318f299d46f93d6fe9d
Ethan Desilets <ethandesilets@gmail.com>
2026-02-13 15:46:28 -0500

    Modify Icon container

--- app/components/media/Icon.tsx
+++ app/components/media/Icon.tsx
@@ -2,7 +2,9 @@ import landworksColor from '../../assets/landworks-color.png';
 
 export function Icon({ }: {}) {
   return (
-      <img src={landworksColor} alt="Landworks Logo" style={{ maxWidth: '15%', maxHeight: '15%'}} />
+    <div className='container-icon' style={{ width:'20%', height: '2em', border: '2px solid red', overflow: 'visible'}}>
+      <img src={landworksColor} alt="Landworks Logo" style={{ maxWidth: '100%', height:'auto'}} />
+    </div>
   );
 }
 
